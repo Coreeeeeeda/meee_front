8791caa4637a4ef020af8ba94a56569d665b4353
Mizuki Koreeda <this.branch.dev@gmail.com>
2022-06-10 19:54:07 +0900

    Fix Vuetify Module

--- pages/index.vue
+++ pages/index.vue
@@ -1,103 +1,105 @@
 <template>
-  <v-container fluid>
-    <v-card
-      flat
-      tile
-      color="transparent"
-    >
-      <v-card-title>
-        Usersテーブルの取得
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table dense>
-          <template
-            v-if="users.length"
-            v-slot:default
-          >
-            <thead>
-              <tr>
-                <th
-                  v-for="(key, i) in userKeys"
-                  :key="`key-${i}`"
+  <v-app>
+    <v-container fluid>
+      <v-card
+        flat
+        tile
+        color="transparent"
+      >
+        <v-card-title>
+          Usersテーブルの取得
+        </v-card-title>
+        <v-card-text>
+          <v-simple-table dense>
+            <template
+              v-if="users.length"
+              v-slot:default
+            >
+              <thead>
+                <tr>
+                  <th
+                    v-for="(key, i) in userKeys"
+                    :key="`key-${i}`"
+                  >
+                    {{ key }}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(user, i) in users"
+                  :key="`user-${i}`"
                 >
-                  {{ key }}
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(user, i) in users"
-                :key="`user-${i}`"
-              >
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ dateFormat(user.created_at) }}</td>
-              </tr>
-            </tbody>
-          </template>
-          <template v-else>
-            ユーザーが存在しません
-          </template>
-        </v-simple-table>
-      </v-card-text>
-      <v-card-title>
-        Vuetifyの導入（オリジナルカラーの確認）
-      </v-card-title>
-      <v-card-text>
-        <v-btn
-          v-for="(color, i) in colors"
-          :key="`color-${i}`"
-          :color="color"
-          class="mr-2"
-        >
-          {{ color }}
-        </v-btn>
-      </v-card-text>
-      <v-card-title>
-        VuetifyカスタムCSSの検証
-      </v-card-title>
-      <v-card-text>
-        ipad（768px）とmobile（426px）で表示・非表示
-      </v-card-text>
-      <v-card-text>
-        <v-card
-          v-for="(cls, i) in customClass"
-          :key="`cls-${i}`"
-          :color="cls.color"
-          :class="cls.name"
-        >
-          <v-card-text>
-            {{ cls.des }}
-          </v-card-text>
-        </v-card>
-      </v-card-text>
-      <v-card-title>
-        nuxt-i18nの検証
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table dense>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th>en</th>
-                <th>ja</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(path, i) in ['signup', 'login']"
-                :key="`path-${i}`"
-              >
-                <td>{{ path }}</td>
-                <td>{{ $t(`title.${path}`) }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-card-text>
-    </v-card>
-  </v-container>
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                  <td>{{ dateFormat(user.created_at) }}</td>
+                </tr>
+              </tbody>
+            </template>
+            <template v-else>
+              ユーザーが存在しません
+            </template>
+          </v-simple-table>
+        </v-card-text>
+        <v-card-title>
+          Vuetifyの導入（オリジナルカラーの確認）
+        </v-card-title>
+        <v-card-text>
+          <v-btn
+            v-for="(color, i) in colors"
+            :key="`color-${i}`"
+            :color="color"
+            class="mr-2"
+          >
+            {{ color }}
+          </v-btn>
+        </v-card-text>
+        <v-card-title>
+          VuetifyカスタムCSSの検証
+        </v-card-title>
+        <v-card-text>
+          ipad（768px）とmobile（426px）で表示・非表示
+        </v-card-text>
+        <v-card-text>
+          <v-card
+            v-for="(cls, i) in customClass"
+            :key="`cls-${i}`"
+            :color="cls.color"
+            :class="cls.name"
+          >
+            <v-card-text>
+              {{ cls.des }}
+            </v-card-text>
+          </v-card>
+        </v-card-text>
+        <v-card-title>
+          nuxt-i18nの検証
+        </v-card-title>
+        <v-card-text>
+          <v-simple-table dense>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th>en</th>
+                  <th>ja</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(path, i) in ['signup', 'login']"
+                  :key="`path-${i}`"
+                >
+                  <td>{{ path }}</td>
+                  <td>{{ $t(`title.${path}`) }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
